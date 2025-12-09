52700c42586f427160cc8bcdaa7130f105b1fb59
Nethmi Wijayasinghe <nethmiwijayasingha@gmail.com>
2025-12-09 11:43:53 +0530

    format document

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -10,35 +10,35 @@ export default function Footer() {
     <footer className="footer">
       <div className="top f-section">
         <div className="rf-section" >
-        <div className="logo">Qureos</div>
-        <div >
-          <ul className="no-bullets">
-            <li>1-on-1 Coaching</li>
-            <li>Live Sessions</li>
-            <li>Blog</li>
-            <li>Careers at Qureos</li>
-          </ul>
-        </div>
-        </div>
-        <div className="rf-section">
-        <div >
-          <ul className="no-bullets">
-            <li>Jobs</li>
-            <li>For Mentors</li>
-            <li>For Employers</li>
-          </ul>
-        </div>
-        <div >
-          <p>Social Media</p>
-          <div className="no4">
-            <div className="s-box"><img src={instagram} alt="instagram" /></div>
-            <div className="s-box"><img src={twitter} alt="twitter" /></div>
-            <div className="s-box"><img src={linkedIn} alt="linkedIn" /></div>
-            <div className="s-box"><img src={youtube} alt="youtube" /></div>
-            <div className="s-box"><img src={tiktok} alt="tiktok" /></div>
-            <div className="s-box"><img src={facebook} alt="facebook" /></div>
+          <div className="logo">Qureos</div>
+          <div >
+            <ul className="no-bullets">
+              <li>1-on-1 Coaching</li>
+              <li>Live Sessions</li>
+              <li>Blog</li>
+              <li>Careers at Qureos</li>
+            </ul>
           </div>
         </div>
+        <div className="rf-section">
+          <div >
+            <ul className="no-bullets">
+              <li>Jobs</li>
+              <li>For Mentors</li>
+              <li>For Employers</li>
+            </ul>
+          </div>
+          <div >
+            <p>Social Media</p>
+            <div className="no4">
+              <div className="s-box"><img src={instagram} alt="instagram" /></div>
+              <div className="s-box"><img src={twitter} alt="twitter" /></div>
+              <div className="s-box"><img src={linkedIn} alt="linkedIn" /></div>
+              <div className="s-box"><img src={youtube} alt="youtube" /></div>
+              <div className="s-box"><img src={tiktok} alt="tiktok" /></div>
+              <div className="s-box"><img src={facebook} alt="facebook" /></div>
+            </div>
+          </div>
         </div>
       </div>
       <div className="bottom"><p>© 2024 Qureos. All rights reserved.</p></div>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,35 +2,35 @@ import "./Header.css";
 import SearchIcon from "@mui/icons-material/Search";
 
 export default function Navbar() {
-    return (
-        <nav className="navbar">
-            <div className="nav-left">
-                <div className="nav-logo">Qureos</div>
+  return (
+    <nav className="navbar">
+      <div className="nav-left">
+        <div className="nav-logo">Qureos</div>
 
-                <ul className="nav-links">
-                    <li>
-                        <select className="member-select">
-                            <option>For Members</option>
-                            <option>For Employees</option>
-                            <option>For Students</option>
-                        </select>
-                    </li>
-                    <li>Jobs</li>
-                </ul>
-            </div>
-            
-            <div className="nav-right">
-                <div className="search-box">
-                    <input
-                        type="text"
-                        placeholder="Search by topic"
-                        className="search-input"
-                    />
-                    <SearchIcon className="search-icon" />
-                </div>
-                <li className="login">Login</li>
-                <button className="nav-btn">Become a member</button>
-            </div>
-        </nav>
-    );
+        <ul className="nav-links">
+          <li>
+            <select className="member-select">
+              <option>For Members</option>
+              <option>For Employees</option>
+              <option>For Students</option>
+            </select>
+          </li>
+          <li>Jobs</li>
+        </ul>
+      </div>
+
+      <div className="nav-right">
+        <div className="search-box">
+          <input
+            type="text"
+            placeholder="Search by topic"
+            className="search-input"
+          />
+          <SearchIcon className="search-icon" />
+        </div>
+        <li className="login">Login</li>
+        <button className="nav-btn">Become a member</button>
+      </div>
+    </nav>
+  );
 }
