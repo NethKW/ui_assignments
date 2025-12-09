77e8bfb2edd53c48a5330c9dbd208e3d1fc166a0
Nethmi Wijayasinghe <nethmiwijayasingha@gmail.com>
2025-12-09 18:48:47 +0530

    update with logo section

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,14 @@
 import Navbar from "../components/Header";
 import Footer from "../components/Footer";
 import Member_section from "../components/member_section";
+import Logo_section from "../components/logo-section"
 
 export default function Home() {
   return (
     <>
       <Navbar />
       <Member_section />
+      <Logo_section />
       <Footer />
     </>
   );
