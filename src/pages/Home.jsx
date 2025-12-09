a5738a37cf64324d8290f9c066850f3d22f2ae59
Nethmi Wijayasinghe <nethmiwijayasingha@gmail.com>
2025-12-09 11:27:07 +0530

    create and update home page

---
+++ src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import Navbar from "../components/Header";
+import Footer from "../components/Footer";
+import Member_section from "../components/member_section";
+
+export default function Home() {
+  return (
+    <>
+      <Navbar />
+      <Member_section />
+      <Footer />
+    </>
+  );
+}
