718b2fbd1ba297de3ae050f4705eb84290d867fa
Nethmi Wijayasinghe <nethmiwijayasingha@gmail.com>
2025-12-23 01:35:49 +0530

    update signup section with mobile view responsive

---
+++ src/components/SignUp.jsx
@@ -0,0 +1,10 @@
+import "../components/SignUp.css";
+
+export default function SignUp() {
+    return (
+        <div className="signup">
+            <h1> Kick start your career in <br />Tech with just one click!</h1>
+            <button className="s-btn">Sign me up</button>
+        </div>
+    );
+}
